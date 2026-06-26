77dc8d95df68b42a79b464b92299e1dd00eb11fa
krish <kp526757@gmail.com>
2026-06-26 14:25:54 +0530

    Initial commit

---
+++ src/p11.kt
@@ -0,0 +1,25 @@
+fun main ()
+{
+    var i=200
+    var j:Int=100
+    var f: Float=2.5f
+    var c:Char='H'
+    var s:String="Abc"
+    var b: Boolean=false
+    var d: Double=96.91
+    var l: Long=9087837
+    var x:Short= 1
+    var by: Byte=124
+    val z=20000
+    println("Integer value= "+i)
+    println("Integer value= "+j)
+    println("Float value= "+f)
+    println("Character Value= "+c)
+    println("String value= "+s)
+    println("Boolean value= "+b)
+    println("Double value= "+d)
+    println("Long value= "+l)
+    println("Short value= "+x)
+    println("Byte value= "+by)
+    println(String.format("z=%d",z))
+}
---
+++ src/p12.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    val i=30000
+    val j=i.toDouble()
+    println("i=$i")
+    println("i=$j")
+    val s1="30500"
+    val s2=s1.toInt()
+    println("s1=$s1,s2=$s2")
+    val k="30000000"
+    val d=k.toDouble()
+    val a=k.toInt()
+    println("k=$k,d=$d,a=$a")
+}
