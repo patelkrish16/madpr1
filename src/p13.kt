4fb45e3d550a353b950abb9e4c73b6137c5a8260
krish <kp526757@gmail.com>
2026-07-02 13:49:00 +0530

    intial

--- src/p13.kt
+++ src/p13.kt
@@ -9,27 +9,27 @@ fun main() {
     println("Enrollment No: $s2")
 
     println("Student Name:")
-    val s3 = readln()
+    val s3 = readln().toString()
     println("Name: $s3")
 
     println("Branch:")
-    val s4 = readln()
+    val s4 = readln().toString()
     println("Branch: $s4")
 
     println("Class:")
-    val s5 = readln()
+    val s5 = readln().toString()
     println("Class: $s5")
 
     println("Batch:")
-    val s6 = readln()
+    val s6 = readln().toString()
     println("Batch: $s6")
 
     println("Student College Name:")
-    val s7 = readln()
+    val s7 = readln().toString()
     println("Student College Name: $s7")
 
     println("Student University Name:")
-    val s8 = readln()
+    val s8 = readln().toString()
     println("Student University Name: $s8")
 
     println("Student Age:")
